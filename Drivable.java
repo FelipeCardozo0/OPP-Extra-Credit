10d60bc3959b1c1907080eedf721f75bde341b4f
Felipe Cardozo <focardo@emory.edu>
2025-03-07 15:24:10 -0500

    Update Drivable.java

--- Drivable.java
+++ Drivable.java
@@ -1,1 +1,11 @@
 
+/*
+THIS CODE WAS MY OWN WORK, IT WAS WRITTEN WITHOUT CONSULTING
+CODE WRITTEN BY OTHER STUDENTS OR COPIED FROM ONLINE RESOURCES. Philip Cardozo
+*/
+
+// Interface defining essential driving behaviors, starting and stopping because we know "cars do not break if the pilot wants to win" ~Lightning McQueen
+interface Drivable {
+    void start(); // Starts the vehicle
+    void stop();  // Stops the vehicle
+}
