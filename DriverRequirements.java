6fdb486fcd952744dbb4c77a214fb7e47d5195fa
Felipe Cardozo <focardo@emory.edu>
2025-03-07 15:23:47 -0500

    Update DriverRequirements.java

--- DriverRequirements.java
+++ DriverRequirements.java
@@ -1,1 +1,9 @@
 
+/*
+THIS CODE WAS MY OWN WORK, IT WAS WRITTEN WITHOUT CONSULTING
+CODE WRITTEN BY OTHER STUDENTS OR COPIED FROM ONLINE RESOURCES. Philip Cardozo
+*/
+
+interface DriverRequirements {
+    boolean canDrive(); // Just to check if the driver is not underage or drunk, meeting all legal requirements
+}
